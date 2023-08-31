5797f1ac64f35e90472e30858285cd21a7fd543b
Seonghwan Park <psh13179578@gmail.com>
2023-09-01 01:52:16 +0900

    news API header ongoing

---
+++ news_times/main.js
@@ -0,0 +1,16 @@
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "25px";
+};
+
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+    let inputArea = document.getElementById("input-area");
+    if (inputArea.style.display === "inline") {
+      inputArea.style.display = "none";
+    } else {
+      inputArea.style.display = "inline";
+    }
+  };
