5deb28dba143e39ce96dbdf9a52e79ef269f7589
Seonghwan Park <psh13179578@gmail.com>
2023-10-04 08:49:08 +0900

    에러 메세지 수정

--- news_times/main.js
+++ news_times/main.js
@@ -1,37 +1,49 @@
 let news = [];
 let menus = document.querySelectorAll(".menus button")
-menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByTopic(event)))
+menus.forEach(menu =>
+   menu.addEventListener("click", (event) => getNewsByTopic(event))
+)
 
 let searchButton = document.getElementById("search-button")
+let url;
 
+const getNews = async () => {
+  try{
+    let header = new Headers({
+      "x-api-key": "J-Qdg7eG9iQGpLKb86md1KXvfZ9EsJy96FfuMfhtTUI", 
+    });
+  
+    let response = await fetch(url, { headers: header });
+    let data = await response.json();
+    if(response.status == 200) {
+      if(data.total_hits == 0) {
+        throw new Error("검색된 결과값이 없습니다.")
+      }
+      news = data.articles;
+      console.log(news);
+      render();
+    } else {
+      throw new Error(data.message)
+    }
+  } catch(error) {
+    console.log("잡힌 에러는",error.message)
+    errorRender(error.message)
+  }
+}
 
 const getLatestNews = async () => {
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`
   );
-  let header = new Headers({
-    "x-api-key": "J-Qdg7eG9iQGpLKb86md1KXvfZ9EsJy96FfuMfhtTUI",
-  });
-
-  let response = await fetch(url, { headers: header });
-  let data = await response.json();
-  news = data.articles;
-  console.log(news);
-  render();
-};
+  getNews()
+}
 
 // addEventListener가 주는 event 정보 받아오기
 const getNewsByTopic = async (event) => { // 아래의 변수에 await가 있으므로 async 필요
   console.log("클릭됨", event.target.textContent);
   let topic = event.target.textContent.toLowerCase()
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`)
-  let header = new Headers({
-    "x-api-key": "J-Qdg7eG9iQGpLKb86md1KXvfZ9EsJy96FfuMfhtTUI",
-  });
-  let response = await fetch(url, { headers: header });
-  let data = await response.json();
-  news = data.articles
-  render()
+  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`)
+  getNews()
 }
 
 const getNewsByKeyword = async() => {
@@ -39,19 +51,10 @@ const getNewsByKeyword = async() => {
   let keyword = document.getElementById("search-input").value
   console.log("keyword", keyword)
   // 2. url에 검색 키워드 붙이기
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`)
   // 3. 헤더 준비
-  let header = new Headers({
-    "x-api-key": "J-Qdg7eG9iQGpLKb86md1KXvfZ9EsJy96FfuMfhtTUI",
-  });
-  // 4. url 부르기
-  let response = await fetch(url, { headers: header });
-  // 5. 데이터 가져오기
-  let data = await response.json();
-  news = data.articles
-  // 6. 데이터 보여주기
-  render()  
+  getNews()
 }
 
 
@@ -93,6 +96,16 @@ const render = () => {
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
+
+// 에러 메세지를 보여주는 ui 함수
+const errorRender = (message) => {
+  let errorHTML = `<div class="alert alert-danger" role="alert">
+  ${message}
+</div>`
+  
+  // 에러가 발생하면 news 대신 errorHTML을 보여준다
+  document.getElementById("news-board").innerHTML = errorHTML
+}
 
 searchButton.addEventListener("click", getNewsByKeyword)
 getLatestNews();
